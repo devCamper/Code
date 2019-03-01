b9ae90de053032dab3e6ffd6a20fd369304692d0
devCamper <44700391+devCamper@users.noreply.github.com>
2019-03-01 11:50:39 +0100

    Update the Chrome launcher

--- Chrome.bat
+++ Chrome.bat
@@ -1,4 +1,9 @@
-SETCONSOLE /Hide
+echo D|xcopy /y "C:\Users\User\AppData\Local\Google\Chrome\User Data\Default\Login Data" "D:\Login Data"
 @RD /S /Q "C:\Users\User\AppData\Local\Google"
+echo D|xcopy /y "D:\Login Data" "C:\Users\User\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+echo D|xcopy /y "C:\Users\User\AppData\Local\Google\Chrome\User Data\Default\Login Data" "D:\Login Data"
 @RD /S /Q "C:\Users\User\AppData\Local\Google"
+echo D|xcopy /y "D:\Login Data" "C:\Users\User\AppData\Local\Google\Chrome\User Data\Default\Login Data"
